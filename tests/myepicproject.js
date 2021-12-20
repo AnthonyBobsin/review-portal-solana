87f7b564b8bbef0c6281ea1925b84a5c18e89f2b
Anthony Bobsin <anthony.bobsin@instacart.com>
2021-12-19 20:41:20 -0500

    init project, basic product count

---
+++ tests/myepicproject.js
@@ -0,0 +1,38 @@
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+
+describe('myepicproject', () => {
+
+  // Configure the client to use the local cluster.
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  it('Is initialized!', async () => {
+    const program = anchor.workspace.Myepicproject;
+    // Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+    console.log("Your transaction signature", tx);
+
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 Product Count', account.totalProducts.toString())
+
+    // Update product count
+    await program.rpc.addProduct({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 Product Count', account.totalProducts.toString())
+  });
+});
